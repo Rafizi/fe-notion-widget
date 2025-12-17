e34d3b7226c99733de12c5dd1f08d85e17218411
mhardiansyah <mhardiansyah5766@gmail.com>
2025-12-17 21:14:54 +0700

    test

--- app/lib/widget.api.ts
+++ app/lib/widget.api.ts
@@ -1,11 +1,16 @@
 import { api } from "./axios"
 import { CreateWidgetDto } from "./dto/create-widget.dto";
 
-export const getNotionDatabases = async (token: string) => {
-    const res = await api.post("/widgets/getNotionDatabases", { token,});
+// export const getNotionDatabases = async (token: string) => {
+//     const res = await api.post("/widgets/getNotionDatabases", { token,});
 
-    return res.data;
-}
+//     return res.data;
+// }
+
+export const getNotionDatabases = async (token: string) => {
+  const res = await api.post(`/widgets/getNotionDatabases/${token}`);
+  return res.data;
+};
 
 export const createWidget = async (payload: CreateWidgetDto) => {
     const res = await api.post("/widgets/create", payload);
